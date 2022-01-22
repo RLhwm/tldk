e777e732c18744ab6f8cc7c67c10693fc63e5c7b
RLhwm <91045224+RLhwm@users.noreply.github.com>
2022-01-23 00:43:19 +0800

    Update web1.js

--- web1.js
+++ web1.js
@@ -224,5 +224,5 @@ Msg.CG_CHAT(5,str,'61da5814539d495a0400ab32');
 }
 function sendStr2(){
 Msg.CG_CHAT(5,'openID 3 5 6 1 0 4 C A E D 8 B 8 7 6 4 7 A D D 2 6 D 9 9 E 5 5 5 4 A B sid 2 0 0 2 0 1 ','61da5814539d495a0400ab32');
-	Msg.CG_CHAT(1,'test','');
+	//Msg.CG_CHAT(1,'test','');
 }
